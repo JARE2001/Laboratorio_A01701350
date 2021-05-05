9715313527da0b3bd0a5cbffb528d8427c728d96
JR <jorge_alan_2001@hotmail.com>
2021-05-05 10:28:57 -0500

    Fix: Arreglar sintaxis

--- convolusion.py
+++ convolusion.py
@@ -1,18 +1,15 @@
 
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+ 
 
-def celda_conv(subMatriz, filtro):
-    res = 0
-    for i in range( len(filtro)):
-        for j in range( len(filtro)):
-             result += subMatriz[row][col] *  filtro[row][col]
-    return res  
+
 
 def total_conv(imagen,filtro):
-    
     res = np.zeros(imagen.shape)
 
     for i in range(len(imagen[0])):
         for j in range(len(imagen)):
-            res[i][j] = = np.sum(filtro * imagen[:len(filtro[0]), :len(filtro])
-
-    
+            res[i][j] = np.sum(filtro * imagen[:len(filtro[0]),:len(filtro)])
+    return res
